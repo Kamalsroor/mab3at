84886e739752e880545f99d36956f76bd725c3f3
kamalsroor <teko.2013@gmail.com>
2020-01-24 18:39:49 +0000

    gdfsgdfg

--- app/PurchasesBillDetailSrials.php
+++ app/PurchasesBillDetailSrials.php
@@ -9,7 +9,7 @@ class PurchasesBillDetailSrials extends Model
     //
     protected $fillable = ['purchases_bill_detail_id', 'user_id', 'srialnumper'];
     protected $primaryKey = 'id';
-    protected $table = 'purchases_bills_detail_Srials';
+    protected $table = 'purchases_bills_detail_srials';
 
     public function user()
     {
